0e0c4cdcb3deb759491310a30b992e3b6514f0a8
Gabriel Schmegel <schmegel09@gmail.com>
2021-10-17 14:00:33 -0300

    primeiro commit

---
+++ Aula 2 - variaveis/script.js
@@ -0,0 +1,117 @@
+/*//tipos priumitivos
+
+//BOOLEAN
+var vOuF = false;
+console.log(typeof(vOuF));
+
+//NUMBER
+var numberQualquer = 1; 
+console.log(typeof(numberQualquer))
+
+//STRING
+var nome = 'Gabriel';
+console.log(typeof(nome));*/
+
+
+//COMO DECLARAR
+
+//VAR
+/*var variavel = 'Gabriel'; 
+variavel = 'Moreira';
+console.log(variavel);*/
+
+/*
+//LET
+let variavel2 = 'Gabriel Moreira';
+variavel2 = 'Schmegel';
+console.log(variavel2);*/
+
+//CONST - Não da para alterar a veriavel depois que e atibuida
+/*const constante = 'Gabriel123';
+constante = 123
+console.log(constante);*/
+
+// ESCOPO
+/*var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopolocal(){
+    let escopoLocalInteno = 'Local';
+    console.log(escopoLocalInteno);    
+}
+escopolocal();*/
+
+//ATRIBUIÇÃO
+/*
+var atribuicao = 'Gabriel';
+console.log(atribuicao);
+
+//COMPARAÇÃO
+var comparacao = '0' == 0;
+console.log(comparacao);
+
+//COMPARAÇÃO IDENTICA
+var comparacaoIdentica = '0' === 0 ;
+console.log(comparacaoIdentica);*/
+
+//OPERADORES RELACIONAIS E MATEMATICOS
+/*
+//ADIÇÃO
+var adicao = 1 + 1;
+console.log(adicao);
+
+//SUBTRAÇÃO
+var subtracao = 2 - 1;
+console.log(subtracao);
+
+//MULTIPLICACAO
+var multiplicacao = 5 * 2;
+console.log( multiplicacao);
+
+//DIVISÃO REAL
+var divisaoreal = 6 / 2;
+console.log(divisaoreal);
+
+//DIVISÃO INTEIRA
+var divisaoInteira = 5 % 3;
+console.log(divisaoInteira);
+
+//POTENCIAÇÃO
+var portenciacao = 2**10;
+console.log(portenciacao);
+
+//MAIOR QUE ,
+
+var maiorque = 5 > 2;
+console.log(maiorque);
+
+//MENOR QUE 
+
+var menorque = 5 < 2;
+console.log(menorque);
+
+//MAIOR OU IGUAL 
+
+var maiorouigual = 5 >= 2;
+console.log(maiorouigual);
+
+//MENOR OU IGUAL
+
+var menorouigual = 5 <= 2;
+console.log(menorouigual);
+
+
+//OPERADORES LOGICOS
+
+//E
+var e = true  && false;
+console.log(e);
+
+//OU
+var ou = false || true;
+console.log(ou);
+//NÃO
+
+var nao = !false;
+console.log(nao);
+*/
